d4418ab75aa891ef86336edd8c8af9decf97bf8d
Anshu Sharma <anshu.sharma@truebil.com>
2020-04-25 17:25:06 +0530

    Article extraction logic added

--- pratilipi_scraper.py
+++ pratilipi_scraper.py
@@ -1,28 +1,75 @@
+import time
+from urllib.parse import urlencode
+
 import requests
 
 
 class PratilipiSpider:
     base_url = 'https://hindi.pratilipi.com/'
+    params = {
+        'language': 'HINDI',
+        'offset': 0,
+        'category': '',
+        'fromSec': 300,
+        'toSec': 1799
+    }
 
-    def __init__(self):
+    headers = {
+        "Cookie": ""
+    }
+
+    def __init__(self, language):
         self.session = requests.Session()
+        self.params['language'] = language
 
-    def get_trending_articles(self):
-        pass
+    def get_sorted_articles(self, category, url, retry_count):
+        retry = 0
+        offset = 0
+        articles = []
+        display_titles = []
+        params = dict(self.params)
+        params['category'] = category
+
+        while retry < retry_count:
+            response = requests.get(url + urlencode(params), headers=self.headers)
+            while response.status_code != 404:
+                data = response.json()
+                if data['pratilipiList']:
+                    for article in data['pratilipiList']:
+                        if article['displayTitle'] not in display_titles:
+                            display_titles.append(article['displayTitle'])
+                            articles.append(article)
+
+                    offset += 20
+                    params.update({
+                        'offset': offset
+                    })
+                    next_url = url + urlencode(params)
+                    response = self.session.get(next_url, headers=self.headers)
+                else:
+                    break
+            retry += 1
+            time.sleep(3)
+
+        return articles
 
     def get_popular_articles(self):
-        pass
+        high_rated_url = 'https://hindi.pratilipi.com/api/stats/v2.0/high_rated?'
+        articles = self.get_sorted_articles('lovestories', high_rated_url, 1)
+        print('POPULAR', len(articles))
+
+        return articles
 
     def get_recent_articles(self):
-        pass
+        recent_url = 'https://hindi.pratilipi.com/api/stats/v2.0/recent_published?'
+        articles = self.get_sorted_articles('lovestories', recent_url, 3)
+        print('RECENT', len(articles))
 
-    def parse_articles(self, category):
-        category_url = self.base_url + category
-        response = self.session.get(category_url)
+        return articles
 
     def __str__(self):
         return 'Pratilipi Spider'
 
 
-if __name__ == 'main':
-    crawler = PratilipiSpider()
+if __name__ == "__main__":
+    crawler = PratilipiSpider('HINDI')
---
+++ sql_snippet.sql
@@ -0,0 +1,31 @@
+CREATE TABLE pratilipi_scripts
+(
+    id              INT AUTO_INCREMENT,
+    title           VARCHAR(100),
+    read_count      INT       DEFAULT 0,
+    language        VARCHAR(50),
+    rating          FLOAT     DEFAULT NULL,
+    author_id       INT,
+    pratilipi_id    BIGINT,
+    page_url        TEXT,
+    site_updated_at TIMESTAMP DEFAULT NULL,
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_author FOREIGN KEY (author_id)
+        REFERENCES pratilipi_authors (id)
+);
+
+CREATE TABLE pratilipi_authors
+(
+    id                   INT AUTO_INCREMENT,
+    name                 VARCHAR(100),
+    follow_count         INT         DEFAULT 0,
+    read_count           INT         DEFAULT 0,
+    language             VARCHAR(50) DEFAULT NULL,
+    gender               VARCHAR(10) DEFAULT NULL,
+    pratilipi_id         BIGINT,
+    page_url             TEXT,
+    site_registration_at TIMESTAMP   DEFAULT NULL,
+    created_at           TIMESTAMP   DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
